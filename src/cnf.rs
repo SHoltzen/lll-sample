dca856255d3a5246603ccaf6ecb3787599ddad2c
Steven Holtzen <sholtzen@gmail.com>
2020-04-10 17:37:54 -0700

    looks pretty good and uniform

--- src/cnf.rs
+++ src/cnf.rs
@@ -1,9 +1,22 @@
-use std::cmp::{min, max};
+use std::cmp::*;
 use rand;
+use bitvec::prelude::*;
 use rand::distributions::IndependentSample;
 use rand::StdRng;
 use std::collections::{HashSet, HashMap};
 use std::iter::FromIterator;
+
+macro_rules! set {
+    ( $( $x:expr ),* ) => {  // Match zero or more comma delimited items
+        {
+            let mut temp_set = HashSet::new();  // Create a mutable HashSet
+            $(
+                temp_set.insert($x); // Insert each item matched into the HashSet
+            )*
+                temp_set // Return the populated HashSet
+        }
+    };
+}
 
 
 
@@ -57,32 +70,26 @@ impl Assignment {
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct Cnf {
     clauses: Vec<Vec<Literal>>,
-    deps: Vec<HashSet<usize>>, // for each clause, a vector containing all clauses that share variables with it
+    deps: Vec<BitVec>, // for each clause, a vector containing all clauses that share variables with it
     num_vars: usize,
 }
 
 impl Cnf {
-    fn gen_deps(clauses: &Vec<Vec<Literal>>) -> Vec<HashSet<usize>> {
+    fn gen_deps(clauses: &Vec<Vec<Literal>>) -> Vec<BitVec> {
         let mut r = Vec::new();
         for c1 in 0..(clauses.len()) {
-            let mut s = HashSet::new();
+            let mut s = bitvec![0; clauses.len()];
             for c2 in 0..(clauses.len()) {
-                if c1 == c2 {
-                    continue;
-                }
                 for lit1 in &clauses[c1] {
                     for lit2 in &clauses[c2] {
                         if lit1.get_label() == lit2.get_label() {
-                            s.insert(c2);
+                            s.set(c2, true);
                         }
                     }
                 }
             }
-            println!("length of clause {:?}: {:?}", clauses[c1], s.len());
             r.push(s);
         }
-        let tot = r.iter().fold(0, |sum, val| sum + val.len());
-        println!("average degree: {:?}", (tot as f64) / (r.len() as f64));
         r
     }
 
@@ -138,24 +145,17 @@ impl Cnf {
 
     /// expands a set of clauses to its neighborhood
     fn expand(&self, clauseset: &HashSet<usize>) -> HashSet<usize> {
-        let mut r = HashSet::new();
+        let mut r = bitvec![0; self.clauses.len()];
         for c in clauseset {
-            r = r.union(&self.deps[*c]).cloned().collect();
+            r = r | self.deps[*c].clone();
         }
-        /*
-        let mut visited = HashSet::new();
-        let mut frontier = Vec::from_iter(clauseset.into_iter());
-        while !frontier.is_empty() {
-            let cur = frontier.pop().unwrap();
-            if visited.contains(cur) {
-                continue;
+        let mut res = HashSet::new();
+        for i in 0..self.clauses.len() {
+            if r[i] == true && !clauseset.contains(&i) {
+                res.insert(i);
             }
-            visited.insert(cur);
-            for neighbor in self.deps[*cur] {
-                
-            }
-        }*/
-        r
+        }
+        res
     }
 
     /// gets the indices of all variables in `clauseset`
@@ -173,18 +173,19 @@ impl Cnf {
     fn is_blocked(&self, assgn: &Assignment, restriction: &HashSet<usize>, clause: usize) -> bool {
         // check if they are independent or if the clause is implied
         let mut indep = true;
-        let mut implies = false;
+        let mut implied = false;
+        // println!("checking if {:?} blocks {:?} under restriction {:?}", assgn, &self.clauses[clause], restriction);
         for i in &self.clauses[clause] {
+            // println!("checking {:?}", i);
             let VarLabel(l) = i.get_label();
             if restriction.contains(&(l as usize)) {
                 indep = false;
-                // check if it is implied
                 if assgn.v[l as usize] == i.get_polarity() {
-                    implies = true;
+                    implied = true; // they agree on an instance or they are non-overlapping
                 }
             }
         }
-        return indep || implies;
+        return indep || implied;
     }
 
     /// Produces a clause set that is in the neighborhood of `set` wrt. `assgn`
@@ -197,11 +198,11 @@ impl Cnf {
         let mut u : HashSet<usize> = self.expand(&r);
         while !u.is_empty() {
             let restr = self.var(&r);
-            for i in u {
-                if self.is_blocked(&assgn, &restr, i) {
-                    r.insert(i);
+            for i in &u {
+                if !self.is_blocked(&assgn, &restr, *i) {
+                    r.insert(*i);
                 } else {
-                    n.insert(i);
+                    n.insert(*i);
                 }
             }
             u = self.expand(&r).difference(&n).cloned().collect();
@@ -218,12 +219,11 @@ impl Cnf {
             // println!("assignment: {:?}, bad: {:?}", assgn, self.find_bad(&assgn));
             let resample = self.find_resample(&assgn);
             let vars = self.var(&resample);
-            // println!("resampling size {:?} out of {:?}", vars.len(), assgn.v.len());
+            println!("resampling size {:?} out of {:?}", vars.len(), assgn.v.len());
             assgn = assgn.rerandomize(vars);
             is_bad = self.find_bad(&assgn).len() > 0;
             num += 1;
         }
-        // println!("resampled {:?} times", num);
         assgn
     }
 
@@ -279,27 +279,99 @@ impl Cnf {
 
 
 
+static C0: &'static str = "
+p cnf 227 713
+1 2 0
+2 3 0
+3 4 0
+4 5 0
+";
+
+
 static C1_A: &'static str = "
 p cnf 227 713
 -9 -47 0
--20 -47 0
-47 9 20  0
--47 29 0
-9 0
 ";
+
+// static C8: &'static str = "
+// ";
+
+#[test]
+fn test_expand() {
+    let cnf = Cnf::from_file(String::from(C0));
+    let clauseset = set![0];
+    let r = cnf.expand(&clauseset);
+    assert_eq!(r, set![1]);
+
+    let clauseset2 = set![0, 1];
+    let r = cnf.expand(&clauseset2);
+    assert_eq!(r, set![2]);
+
+    let clauseset3 = set![0, 3];
+    let r = cnf.expand(&clauseset3);
+    assert_eq!(r, set![1, 2]);
+}
+
+#[test]
+fn is_blocked() {
+    let cnf = Cnf::from_file(String::from(C0));
+    let assgn = Assignment{v: vec![false, false, true, true, true]};
+    let restriction = set![0, 1];
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 1), false);
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 2), true);
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 3), true);
+
+    let assgn = Assignment{v: vec![false, true, false, true, true]};
+    let restriction = set![0, 2];
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 1), false);
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 2), false);
+    assert_eq!(cnf.is_blocked(&assgn, &restriction, 3), true);
+}
+
+#[test]
+fn test_bad() {
+    let cnf = Cnf::from_file(String::from(C0));
+    let assgn = Assignment{v: vec![false, false, true, true, true]};
+    let r = cnf.find_bad(&assgn);
+    assert_eq!(set![0], r);
+
+    let assgn = Assignment{v: vec![true, false, false, false, false]};
+    let r = cnf.find_bad(&assgn);
+    assert_eq!(set![1, 2, 3], r);
+}
+
+#[test]
+fn test_var() {
+    let cnf = Cnf::from_file(String::from(C0));
+    let clauseset = set![0, 3];
+    assert_eq!(cnf.var(&clauseset), set![0, 1, 3, 4]);
+}
 
 #[test]
 fn test_resample() {
-    let cnf = Cnf::from_file(String::from(C1_A));
-    // let cnf = Cnf::rand_cnf(&mut StdRng::new().unwrap(), 300, 500);
+    let cnf = Cnf::from_file(String::from(C0));
+    let assgn = Assignment{v: vec![false, false, true, true, true]};
+    assert_eq!(cnf.find_resample(&assgn), set![0, 1]);
+
+    let assgn = Assignment{v: vec![true, false, false, false, false]};
+    assert_eq!(cnf.find_resample(&assgn), set![0, 1, 2, 3]);
+}
+
+
+
+#[test]
+fn test_partial_reject() {
+    // let cnf = Cnf::from_file(String::from(C8));
+    let cnf = Cnf::rand_cnf(&mut StdRng::new().unwrap(), 10, 10);
     let mut results = HashMap::new();
-    for _ in 0..1 {
+    for _ in 0..10000 {
         let res = cnf.partial_rejection();
         *results.entry(res.clone()).or_insert(0) += 1;
-        println!("found sample")
+        println!("found sample\n\n")
     }
 
     for (key, value) in &results {
         println!("{:?}", value);
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate rand;
 mod cnf;
 
 
 fn main() {
     println!("Hello, world!");
 }
+
 
